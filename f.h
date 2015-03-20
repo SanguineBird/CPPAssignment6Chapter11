650584f4d9131819e53b2975e0313aee90051c05
SanguineBird <SanguineBird@users.noreply.github.com>
2015-03-20 17:42:44 -0400

    Create f.h
    untested

---
+++ f.h
@@ -0,0 +1,15 @@
+/*
+This is a very simple header file to demonstrate encapsulation techniques.
+*/
+
+#ifndef F_H
+#define F_H
+
+namespace FMMoore{
+  class FClass{
+    public:
+      void hello();
+      //prints a message to screen
+  };
+} //end namespace FMMoore
+#endif //F_H
