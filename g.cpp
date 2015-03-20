317cc07f15f39af676903311889224a46f48d3bb
SanguineBird <SanguineBird@users.noreply.github.com>
2015-03-20 17:44:18 -0400

    Create g.cpp
    untested

---
+++ g.cpp
@@ -0,0 +1,14 @@
+/*
+This is a very simple implementation file to demonstrate encapsulation techniques. It is for the class GClass.
+*/
+
+#include <iostream> //cout
+using std::cout;
+#include "g.h"
+
+namespace GMMoore{
+  //uses cout:
+  void hello(){
+    cout << "Hello from g.\n";
+  }
+} //end namespace GMMoore
