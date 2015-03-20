7a0d686338487adaf622fb58847c4bceccc4e882
SanguineBird <SanguineBird@users.noreply.github.com>
2015-03-20 17:43:20 -0400

    Create f.cpp
    untested

---
+++ f.cpp
@@ -0,0 +1,14 @@
+/*
+This is a very simple implementation file to demonstrate encapsulation techniques. It is for the class FClass.
+*/
+
+#include <iostream> //cout
+using std::cout;
+#include "f.h"
+
+namespace FMMoore{
+  //uses cout:
+  void hello(){
+    cout << "Hello from f.\n";
+  }
+} //end namespace FMMoore
