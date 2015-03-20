7678a2165f6e522bd9f3142b7643ebde50b19f1b
SanguineBird <SanguineBird@users.noreply.github.com>
2015-03-20 17:43:47 -0400

    Create g.h
    untested

---
+++ g.h
@@ -0,0 +1,15 @@
+/*
+This is a very simple header file to demonstrate encapsulation techniques.
+*/
+
+#ifndef G_H
+#define G_H
+
+namespace GMMoore{
+  class GClass{
+    public:
+      void hello();
+      //prints a message to screen
+  };
+} //end namespace GMMoore
+#endif //G_H
